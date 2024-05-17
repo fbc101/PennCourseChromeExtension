de2c0d7e072374ff836f5b5bf9c70c0329cff378
Franci Branda-CHen <fbc101@seas.upenn.edu>
2024-05-17 13:53:23 -0400

    error handling

--- public/background.js
+++ public/background.js
@@ -9,24 +9,28 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 
   // if a course was found, proceed to clean it
   if (match) {
-    // On the first course match, replace all occurrences of any symbols with a space
-    const noSymbolStringTrimmed = match[0].replace(regex, ' ').trim().toLocaleUpperCase();
-    // splits by space and &nbsp; 
-    const codeAndNumber = noSymbolStringTrimmed.split(/\s|&nbsp;/);
-
-    // Check if the number part has 3 digits, and if so, append a '0'
-    if (codeAndNumber[1].length === 3) {
-      codeAndNumber[1] += '0';
+    try {
+      // On the first course match, replace all occurrences of any symbols with a space
+      const noSymbolStringTrimmed = match[0].replace(regex, ' ').trim().toLocaleUpperCase();
+      // splits by space and &nbsp; 
+      const codeAndNumber = noSymbolStringTrimmed.split(/\s|&nbsp;/);
+  
+      // Check if the number part has 3 digits, and if so, append a '0'
+      if (codeAndNumber[1].length === 3) {
+        codeAndNumber[1] += '0';
+      }
+  
+      const cleanedInputCourse = codeAndNumber[0] + '-' + codeAndNumber[1]; // Code-Number e.g CIS-1200
+  
+      const newinput = {
+        inputcourse: cleanedInputCourse
+      };
+  
+      chrome.storage.local.set(newinput, () => {
+        console.log('Input course saved');
+      });
+    } catch (error) {
+      console.log(error);
     }
-
-    const cleanedInputCourse = codeAndNumber[0] + '-' + codeAndNumber[1]; // Code-Number e.g CIS-1200
-
-    const newinput = {
-      inputcourse: cleanedInputCourse
-    };
-
-    chrome.storage.local.set(newinput, () => {
-      console.log('Input course saved');
-    });
   }
 });
