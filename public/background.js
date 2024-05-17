bdd328d44a17bb91ca278b69bbab02cc790cdbfa
Franci Branda-CHen <fbc101@seas.upenn.edu>
2024-05-17 13:33:54 -0400

    cleaner code, no flag needed

--- public/background.js
+++ public/background.js
@@ -1,35 +1,30 @@
 // cleans the highlighted text to retrieve the course_id, otherwise ignore the text
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  const safe = true;
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   const selectedText = message.highlightedText;
 
   const regex = /[^\w\s]/g;
   const entireCodeRegex = /[a-zA-z]{2,4}([-\s]|(&nbsp;))[0-9]{3,4}/;
 
   const match = entireCodeRegex.exec(selectedText);
-  let codeAndNumber; 
+
   // if a course was found, proceed to clean it
   if (match) {
     // On the first course match, replace all occurrences of any symbols with a space
     const noSymbolStringTrimmed = match[0].replace(regex, ' ').trim().toLocaleUpperCase();
     // splits by space and &nbsp; 
-    codeAndNumber = noSymbolStringTrimmed.split(/\s|&nbsp;/); 
-  } else { // no course was found in the entire highlightedText
-    safe = false;
-  }
-    
-  // Check if the number part has 3 digits, and if so, append a '0'
-  if (codeAndNumber[1].length === 3) {
-    codeAndNumber[1] += '0';
-  } 
-  
-  const cleanedInputCourse = codeAndNumber[0] + '-' + codeAndNumber[1]; // Code-Number e.g CIS-1200
+    const codeAndNumber = noSymbolStringTrimmed.split(/\s|&nbsp;/);
 
-  const newinput = {
-    inputcourse : cleanedInputCourse
-  };
+    // Check if the number part has 3 digits, and if so, append a '0'
+    if (codeAndNumber[1].length === 3) {
+      codeAndNumber[1] += '0';
+    }
 
-  if (safe) {
+    const cleanedInputCourse = codeAndNumber[0] + '-' + codeAndNumber[1]; // Code-Number e.g CIS-1200
+
+    const newinput = {
+      inputcourse: cleanedInputCourse
+    };
+
     chrome.storage.local.set(newinput, () => {
       console.log('Input course saved');
     });
